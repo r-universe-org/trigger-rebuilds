40ecfe824046b47374f07f20ff786c2b57ae9fd2
Jeroen Ooms <jeroenooms@gmail.com>
2021-05-16 14:21:31 +0200

    Relax rebuilding

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 set -e
 echo "Looking for rebuilds in ${1}"
-#Rscript -e "rebuilds::trigger_rebuilds('${1}')"
-Rscript -e "rebuilds::rebuild_missing_binaries(basename('${1}'))"
+Rscript -e "rebuilds::trigger_rebuilds('${1}')"
+#Rscript -e "rebuilds::rebuild_missing_binaries(basename('${1}'))"
 echo "Action complete!"
